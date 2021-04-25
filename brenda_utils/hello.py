223e745ef416eeeece47696ac39d457524dbaf6e
Igor Yaroshenko <iyaroshenko@accesssoftek.com>
2021-04-25 14:16:12 +0300

    external libs

--- brenda_utils/hello.py
+++ brenda_utils/hello.py
@@ -1,5 +1,13 @@
+import pandas as pd
+
+
 def hello_word():
     print("hello world mthfck")
+
+    d = {'col1': [1, 2], 'col2': [3, 4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+
 
 if __name__ == "__main__":
     hello_word()
