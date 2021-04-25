ae27f4f98830650341ac7bbd76b9c032131a2de8
Igor Yaroshenko <iyaroshenko@accesssoftek.com>
2021-04-25 13:05:36 +0300

    pckg setup

--- brenda-utils/hello.py
+++ brenda-utils/hello.py
@@ -1,2 +1,5 @@
 def hello_word():
     print("hello world")
+
+if __name__ == "__main__":
+    hello_word()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='brenda-utils',
+    packages=['brenda-utils'],
+    version='0.1',
+    url='https://github.com/usual1suspect2/brenda-utils',
+    author='brenda',
+    keywords=['pip','brenda-utils']
+    )
