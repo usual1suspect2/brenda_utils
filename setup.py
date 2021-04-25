80890c0b362fcafc58a4f7f6e8d06f9d8ed9876e
Igor Yaroshenko <iyaroshenko@accesssoftek.com>
2021-04-25 14:41:49 +0300

    pckg setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='brenda_utils',
     packages=['brenda_utils'],
     version='0.1',
-    url='https://github.com/usual1suspect2/brenda-utils',
+    url='https://github.com/usual1suspect2/brenda_utils',
     author='brenda',
     keywords=['pip','brenda-utils']
     )
